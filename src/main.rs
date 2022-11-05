bea6b3186ab272ef871a782f44ba827bf4a39b36
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-11-05 15:30:04 +0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let src = "fello earth";
+    let pig = convert_to_pig_latin(&src);
+    println!("{pig}")
+}
+
+fn convert_to_pig_latin(s: &str) -> String {
+    let mut pig_latin = Vec::new();
+    let vovels = ['a', 'e', 'i', 'o', 'u'];
+    let words = s.split_whitespace();
+    for word in words {
+        let mut pig_word = String::new();
+        if word.starts_with(&vovels) {
+            pig_word.push_str(word);
+            pig_word.push_str("-hay");
+            pig_latin.push(pig_word);
+        } else {
+            let first_char = word.chars().nth(0).unwrap().to_string();
+            let other_part = String::from(word).replace(&first_char, "");
+            pig_word = format!("{}-{}ay", other_part, first_char);
+            pig_latin.push(pig_word);
+        }
+    }
+    pig_latin.join(" ")
 }
